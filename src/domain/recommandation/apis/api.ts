ef9802bc516a566517fa11d8face4f3ef72892b9
ws <cws3299@gmail.com>
2025-06-24 15:12:24 +0900

    develop 확인

--- src/domain/recommandation/apis/api.ts
+++ src/domain/recommandation/apis/api.ts
@@ -1,15 +1,17 @@
-const places = async (location:string, keyword:string = '맛집') => {
+const places = async (location: string, keyword: string = '맛집') => {
   try {
     const response = await fetch(
-      `/.netlify/functions/searchPlaces?location=${encodeURIComponent(location)}&keyword=${encodeURIComponent(keyword)}`
+      `/.netlify/functions/searchPlaces?location=${encodeURIComponent(location)}&keyword=${encodeURIComponent(keyword)}`,
     );
-    
+
     const data = await response.json();
+
+    console.log(data);
     return data;
   } catch (error) {
     console.error('장소 검색 실패:', error);
     return [];
   }
-}
+};
 
-export default places
+export default places;
