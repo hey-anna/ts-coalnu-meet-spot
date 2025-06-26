133af1b4464d6701d2437f9397e07515889007ee
ws <cws3299@gmail.com>
2025-06-27 01:08:00 +0900

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router';
 import AppLayout from './layout/AppLayout';
 import StationMeetResultPage from './Pages/meetup/StationMeetResultPage';
 import TestPage from './Pages/test/TestPage';
-// import MainPage from './Pages/main/MainPage';
+import MainPage from './Pages/main/MainPage';
 
 const JoinPage = React.lazy(() => import('./Pages/auth/JoinPage'));
 const LoginPage = React.lazy(() => import('./Pages/auth/LoginPage'));
@@ -17,7 +17,7 @@ function App() {
     <Suspense>
       <Routes>
         <Route path="/" element={<AppLayout />}>
-          {/* <Route index element={<MainPage />}></Route> */}
+          <Route index element={<MainPage />}></Route>
           <Route path="/join" element={<JoinPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/meetup/result" element={<StationMeetResultPage />} />
